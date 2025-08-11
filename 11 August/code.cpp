f5adeffc3d6ea4cabdd03a06e98d6852822a8338
Nikhilbhanderi91 <nikhilbhanderi4410@gmail.com>
2025-08-11 18:56:43 +0530

    11 August leetcode solution

---
+++ 11 August/code.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    long long modPow(long long base, long long exp) {
+        long long res = 1;
+        while (exp > 0) {
+            if (exp & 1) res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return res;
+    }
+
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        // Step 1: Get powers array
+        vector<int> powers;
+        for (int i = 0; i < 31; i++) {
+            if (n & (1 << i)) {
+                powers.push_back(1 << i);
+            }
+        }
+
+        // Step 2: Build prefix product array
+        int m = powers.size();
+        vector<long long> prefix(m);
+        prefix[0] = powers[0] % MOD;
+        for (int i = 1; i < m; i++) {
+            prefix[i] = (prefix[i-1] * powers[i]) % MOD;
+        }
+
+        // Step 3: Answer queries
+        vector<int> ans;
+        for (auto &q : queries) {
+            int l = q[0], r = q[1];
+            if (l == 0) {
+                ans.push_back((int)prefix[r]);
+            } else {
+                long long inv = modPow(prefix[l-1], MOD - 2);
+                long long val = (prefix[r] * inv) % MOD;
+                ans.push_back((int)val);
+            }
+        }
+        return ans;
+    }
+};
